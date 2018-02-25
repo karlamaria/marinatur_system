14391c598ebd686249bdb293987cd532c3466a4d
Karla Garcia <karlamaria@gmail.com>
2018-02-25 00:04:01 -0300

    Update i18n

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module MTS
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.time_zone = 'Brasilia'
-    config.i18n.default_locale = 'pt-BR'
+    config.i18n.default_locale = :"pt-BR"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
